e40876f80d71f09ef54edf30f7c8b3fa0a55e244
Ivan Kostadinov <ikostadinov93@gmail.com>
2024-03-30 12:33:39 +0200

    add validation

--- netlify/functions/hello/hello.js
+++ netlify/functions/hello/hello.js
@@ -1,9 +1,26 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
+
+
+// Parse the query params
+  let mode = event.queryStringParameters["hub.mode"];
+  let token = revent.queryStringParameters["hub.verify_token"];
+  let challenge = event.queryStringParameters["hub.challenge"];
+
+let statusCode = 200;
+    // Check if a token and mode is in the query string of the request
+   if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === "123-abc-456") {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+    }
+       else { statusCode = 400}
+  }
+
+  return {
+      statusCode,
       body: JSON.stringify({ message: `Hello ${subject}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
